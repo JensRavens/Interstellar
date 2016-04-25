3785de4184bd2b61761684d0be07804809681bb3
Ash Furrow <ash@ashfurrow.com>
2016-04-25 14:50:01 -0400

    Adds merge() to Observable.

--- Sources/Observable.swift
+++ Sources/Observable.swift
@@ -107,4 +107,20 @@ extension Observable {
         subscribe { transform($0).subscribe(observable.update) }
         return observable
     }
+
+    public func merge<U>(merge: Observable<U>) -> Observable<(T,U)> {
+        let signal = Observable<(T,U)>()
+        self.subscribe { a in
+            if let b = merge.peek() {
+                signal.update((a,b))
+            }
+        }
+        merge.subscribe { b in
+            if let a = self.peek() {
+                signal.update((a,b))
+            }
+        }
+
+        return signal
+    }
 }
